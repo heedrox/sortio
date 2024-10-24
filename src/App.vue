d5b6e6c59c430913c85fe1f9bda5c7e5fc8c2983
Jordi Marti <heedrox@gmail.com>
2024-10-24 21:23:45 +0200

    first version of cards

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TheBoard from './components/TheBoard.vue'
+import TheInstructions from './components/TheInstructions.vue'
+import shuffle from './lib/shuffle'
+import { ref } from 'vue'
+
+const i = (instruction, card) => ({ instruction, card })
+const c = cardString =>
+  cards.find(card => card.numero + card.suit.toUpperCase() === cardString)
+
+const cards = [
+  { suit: 'fuego', number: 1, symbol: '*' },
+  //{ suit: 'fuego', number: 2, symbol: '' },
+  { suit: 'fuego', number: 3, symbol: '**' },
+  { suit: 'fuego', number: 4, symbol: '' },
+  { suit: 'fuego', number: 5, symbol: '††' },
+  { suit: 'aire', number: 1, symbol: '' },
+  { suit: 'aire', number: 2, symbol: '***' },
+  { suit: 'aire', number: 3, symbol: '*' },
+  //{ suit: 'aire', number: 4, symbol: '' },
+  // { suit: 'aire', number: 5, symbol: '†††' },
+  { suit: 'agua', number: 1, symbol: '†' },
+  { suit: 'agua', number: 2, symbol: '' },
+  { suit: 'agua', number: 3, symbol: '*' },
+  { suit: 'agua', number: 4, symbol: '*' },
+  { suit: 'agua', number: 5, symbol: '' },
+  // { suit: 'tierra', number: 1, symbol: '' },
+  { suit: 'tierra', numero: 2, symbol: '††' },
+  { suit: 'tierra', numero: 3, symbol: '†' },
+  { suit: 'tierra', numero: 4, symbol: '**' },
+  { suit: 'tierra', numero: 5, symbol: '' },
+]
+shuffle(cards)
+const instructions = [
+  i('Del elemento AGUA, la tercera más pequeña', c('3AGUA')),
+]
+
+const vCards = ref(cards)
+const vInstructions = ref(instructions)
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <TheBoard :cards="vCards"></TheBoard>
+    <TheInstructions :instructions="vInstructions"></TheInstructions>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/TheBoard.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({ cards: Array })
+</script>
+
+<template>
+  <div class="board">
+    <div class="row row-cols-4 card-line">
+      <div class="col" v-for="(card, index) in cards" :key="index">
+        <div :class="['card', card.suit]">
+          <div class="symbol">{{ card.symbol }}</div>
+          <div class="number">{{ card.number }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  max-width: 580px;
+  margin: auto;
+  margin-top: 20px;
+}
+.card {
+  position: relative; /* Necesario para que el símbolo se posicione correctamente dentro de la carta */
+  margin-top: 10px;
+  color: #fff;
+  font-size: 40px;
+  width: 140px;
+  height: 180px;
+  padding: 0.5rem;
+  font-weight: bold;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+}
+
+.symbol {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  font-size: 30px;
+}
+.number {
+  position: absolute;
+  top: 0px;
+  left: 10px;
+  font-size: 40px;
+}
+.fuego {
+  background-color: #b02015;
+  background-image: url(../assets/palos/fuego.png);
+  background-position: center;
+  background-size: cover;
+}
+.aire {
+  background-color: #7c11ce;
+  background-image: url(../assets/palos/aire.png);
+  background-position: center;
+  background-size: cover;
+}
+.agua {
+  background-color: #155db0;
+  background-image: url(../assets/palos/agua.png);
+  background-position: center;
+  background-size: cover;
+}
+.tierra {
+  background-color: #2fb015;
+  background-image: url(../assets/palos/tierra.png);
+  background-position: center;
+  background-size: cover;
+}
+</style>
